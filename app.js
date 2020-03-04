df5fbde76f7953f3b681fb1608ce071bcde37da9
Ainur Rohman <ainurrohman869@gmail.com>
2020-03-04 10:40:58 +0700

    Setting up middlewares & node modules

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+dotenv.config({
+ path: './config.env'
+});
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useCreateIndex: true
+});
+
+// middleware for session
+app.use(session({
+ secret: 'Just a simple login/sign up application.',
+ resave: true,
+ saveUninitialized: true
+}));
+
+// middleware flash messages
+app.use(flash());
+
+// setting middleware globally
+app.use((req, res, next) => {
+ res.locals.success_msg = req.flash(('success_msg'));
+ res.locals.error_msg = req.flash(('error_msg'));
+});
+
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(process.env.PORT, () => {
+ console.log('Connected!!!');
+})
