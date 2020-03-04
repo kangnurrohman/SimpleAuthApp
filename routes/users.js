3bbb7cc52eed3777504438db96037bf994ba5ff5
Ainur Rohman <ainurrohman869@gmail.com>
2020-03-04 15:35:44 +0700

    Sign in & logout using passport

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+
 // Requiring user model
 const User = require('../models/usermodel');
+
+// Checks if user is authenticated
+function isAuthenticatedUser(req, res, next) {
+ if (req.isAuthenticated()) {
+  return next();
+ }
+ req.flash('error', 'Please login first to access this page.');
+ res.redirect('/login');
+}
 
 // GET routes
 router.get('/login', (req, res) => {
@@ -13,11 +23,23 @@ router.get('/signup', (req, res) => {
  res.render('signup');
 });
 
-router.get('/', (req, res) => {
+router.get('/', isAuthenticatedUser, (req, res) => {
  res.render('dashboard');
 });
 
+router.get('/logout', (req, res) => {
+ req.logOut();
+ req.flash('success_msg', 'You have been logged out');
+ res.redirect('/login');
+});
+
 // POST routes
+router.post('/login', passport.authenticate('local', {
+ successRedirect: '/',
+ failureRedirect: '/login',
+ failureFlash: 'Invalid email or password. Try again!!!'
+}));
+
 router.post('/signup', (req, res) => {
  let {
   name,
@@ -29,6 +51,7 @@ router.post('/signup', (req, res) => {
   name,
   email
  };
+
  User.register(userData, password, (err, user) => {
   if (err) {
    req.flash('error_msg', 'ERROR' + err);
